38717305ad5d7d6cfe0d187b9e939000d2545f53
ananyadhananjaya <ananya.dhananjaya1998@gmail.com>
2021-11-02 14:48:04 +0530

    waring removed

--- pages/logs/index.jsx
+++ pages/logs/index.jsx
@@ -7,13 +7,14 @@ export default function Logs({ data }) {
     <>
       <div className="">
         <div className="flex flex-wrap justify-center">
-          {Object.values(data).map((doc) => (
+          {Object.keys(data).map(key => (
             <div className="max-w-sm rounded overflow-hidden shadow-md m-2 w-2/3 font-mono hover:shadow-lg hover:text-gray-600">
               <div className="px-6 py-4">
-                <p>{doc.log}</p>
+                <p key = {key}>{data[key].log}</p>
               </div>
             </div>
           ))}
+          
         </div>
       </div>
     </>
